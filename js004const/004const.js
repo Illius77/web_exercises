f668d3e77d0911d20dca83f1a691d8b2cc8ee579
Illius77 <dehti1@bfh.ch>
2025-09-08 10:37:23 +0200

    demo1

---
+++ js004const/004const.js
@@ -0,0 +1,21 @@
+const num = 0;
+
+console.log(num);
+
+const bool = false;
+
+console.log(bool);
+
+let str = "hello";
+// str = "hallo";
+console.log(str);
+
+const arr = [10, 1, 2, 3];
+console.log(arr);
+
+// arr = ['a', 'b', 'c', 'd']; 
+
+arr[0] = 99;
+arr[1] = 'yo';
+// arr = [true, false];
+console.log(arr);
