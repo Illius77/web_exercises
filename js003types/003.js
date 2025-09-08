9f35bb368a49f6ed9a675dd2b653356416ad930b
Illius77 <dehti1@bfh.ch>
2025-09-08 09:40:12 +0200

    demo1

---
+++ js003types/003.js
@@ -0,0 +1,15 @@
+let num = 99;
+
+num = false;
+console.log(num);
+console.info(typeof num);
+
+num = '0';
+console.log(num);
+console.info(typeof num);
+
+let bool = false;
+console.info('typeof bool →>', typeof bool);
+
+bool = 0;
+console.info('typeof bool →>>', typeof bool);
