3e41acb92b1fa54db3761062a5212cb2d982f41a
Illius77 <dehti1@bfh.ch>
2025-09-08 10:15:40 +0200

    demo1

--- js003types/003.js
+++ js003types/003.js
@@ -1,27 +1,40 @@
-// changing the object type
 let num = 99;
 
 num = false;
-console.log(num); // false
-console.info(typeof num); // boolean
+console.log(num); 
+console.info(typeof num); 
 
 num = '0';
-console.log(num); // '0'
-console.info(typeof num); // string
+console.log(num); 
+console.info(typeof num); 
 
 let bool = false;
-console.info('typeof bool →>', typeof bool); // boolean
+console.info('typeof bool →>', typeof bool); 
 
 bool = 0;
-console.info('typeof bool →>>', typeof bool); // number
+console.info('typeof bool →>>', typeof bool); 
 
 let str = "hello";
-console.log("typeof str -->", typeof str); // string
+console.log("typeof str -->", typeof str); 
 
 let arr = [99, "1", true, 31];
-console.log(arr); // [99, "1", true, 31]
-console.info("typeof arr →>", typeof arr); // object
+console.log(arr); 
+console.info("typeof arr →>", typeof arr); 
 
 arr[0] = 88;
 console.log("arr[0] →>", arr[0]);
 console.log("arr[1] →>", arr[1]);
+
+let obj = {
+  name: "Ronald",
+  age: 34
+};
+console.info("typeof obj →", typeof obj); 
+console.log("obj.age →>>", obj.age); 
+console.info("typeof obj.age →", typeof obj.age); 
+console.log("obj.name →>>", obj.name); 
+console.log("typeof obj.name →>>", typeof obj.name); 
+console.error('ERROR IN RED')
+console.warn
+
+console.log(obj.name, obj.age);
