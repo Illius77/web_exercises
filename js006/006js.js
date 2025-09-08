01dfb54b05a14b4b84152c232d3a897510a3bf9a
Illius77 <dehti1@bfh.ch>
2025-09-08 10:59:32 +0200

    demo1

---
+++ js006/006js.js
@@ -0,0 +1,23 @@
+let num = 0;
+// num = '0'; // <--- Example of assigning a string
+// num = false; // Example of assigning a boolean
+
+console.log(typeof num); // Shows the type of num
+
+let num2 = false;
+console.log('num == false :: ', num == num2);
+console.log('num === false ::', num === num2);
+console.log(num === false); // true
+console.log(num === true); // false
+console.log(num); // false
+console.log(!num); // true
+console.log(num != true);
+console.log(num !== true);
+// let arr = [0, 1, 2, 3];
+// console.log(arr == [0, 1, 2, 3]); // false, different references
+
+let obj = {
+    name: 'Ronald',
+    age: 33
+};
+// console.log(obj);
