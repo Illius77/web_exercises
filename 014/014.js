eede98e254194f51bf1c1f084e11f259949f122c
Illius77 <dehti1@bfh.ch>
2025-09-11 09:56:55 +0200

    demo

---
+++ 014/014.js
@@ -0,0 +1,17 @@
+
+this.onload = ()=>{ 
+
+    const body = document.querySelector('body');
+    
+    const title = document.createElement('h1');
+   
+    title.textContent = 'hello, I am addded by code !';
+    const mySpan = document.createElement("span");
+    mySpan.textContent = "(span)";
+    title.appendChild(mySpan);
+    
+    body.appendChild(title);
+   
+    const unwanted = document.querySelector('.info');
+    
+}
