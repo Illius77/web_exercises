1307a9b2c555a905ab53558abe1d5af103b29ede
Illius77 <dehti1@bfh.ch>
2025-09-24 11:10:42 +0200

    demo

--- art/code.js
+++ art/code.js
@@ -10,8 +10,8 @@ const timeout2 = 500; //EDIT
 const timeout3 = 500; //EDIT
   const primary = "&n7577;"; // Initial char.
 // First 
-const center = ['@', '®', '©',]; //EDIT
-const tertiary = ['.', '°']; //EDIT
+const center = ['@', '!', '©',]; //EDIT
+const tertiary = ['.', '&']; //EDIT
 const secondary = ['\\', '/', '|','&boxv;','&boxh;','&boxvl;','&boxvl;','&boxhd;','&boxhu;']; //EDIT
 const max = 2; // First circle around center.
 const max2 = 3; // 2nd circle around center.
